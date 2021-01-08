59ae71f2b18e222f95ea9505cdf9dc0ca62828c5
firawa_ <fikrirafhsanjani28@gmail.com>
2021-01-08 09:33:33 +0700

    Update into next version

--- gauss_jordan2.py
+++ gauss_jordan2.py
@@ -1,30 +1,38 @@
 import numpy as np
 
-Vektor = np.array([
-    [8, -9, 1, -8, 80],
-    [-3, -1, 5, 4, 7],
-    [-2, -1, -3, 8, -30],
-    [-2, -8, -1, 2, 18]
-], float)
-
-
 def main():
-    [m, n] = np.shape(Vektor)
+    print ("Silahkan tentukan ukuran matriks yang diinginkan?:")
+    m = int(input("Baris: " ))
+    n = int(input("Kolom: "))
+    
+    matrix = np.zeros((m,n))
+    
+    for subscript1 in range (0, m):
+        for subscript2 in range (0, n):
+            print ("Berapakah nilai matriks pada baris {} dan kolom {}".format(subscript1 + 1, subscript2 + 1))
+            matrix[subscript1][subscript2] = int(input())
+            
+    print ("Berikut merupakan matrix yang anda input\n", matrix)
+    
+    print ("=="*30)
+    print ("Gauss Jordan Elimination")
+    print ("=="*30)
+
     for subscript1 in range (0, m - 1):
         for subscript2 in range (subscript1 + 1, m):
-            Vektor[subscript2][:] = Vektor[subscript2][:] - Vektor[subscript1][:] * (Vektor[subscript2][subscript1] / Vektor[subscript1][subscript1])
-            print(Vektor)
-            print('------------------------------------------------------------')
+            matrix[subscript2][:] = matrix[subscript2][:] - matrix[subscript1][:] * (matrix[subscript2][subscript1] / matrix[subscript1][subscript1])
+            print(matrix)
+            print('=='*30)
 
     for subscript3 in range(m - 1, 0, -1):
         for subscript4 in range(subscript3 - 1, -1, -1):
-           Vektor[subscript4][:] = Vektor[subscript4][:] - Vektor[subscript3][:] * (Vektor[subscript4][subscript3] / Vektor[subscript3][subscript3])
-           print (Vektor)
-           print ('------------------------------------------------------------')
+           matrix[subscript4][:] = matrix[subscript4][:] - matrix[subscript3][:] * (matrix[subscript4][subscript3] / matrix[subscript3][subscript3])
+           print (matrix)
+           print ('=='*30)
 
     for subscript5 in range(0, m):
-        Vektor[subscript5][:] = Vektor[subscript5][:] / Vektor[subscript5][subscript5]
-        print (Vektor)
-        print ('---------------------------------------------------------------')
+        matrix[subscript5][:] = matrix[subscript5][:] / matrix[subscript5][subscript5]
+        print (matrix)
+        print ('=='*30)
 
 main()
